dbcb5c34396aa4bf12cf6bfea697ff2e5c3b9f46
Marcelo Moreira <spl1ce5225@gmail.com>
2021-06-20 02:37:11 +0100

    more stuff

--- db/db.py
+++ db/db.py
@@ -4,17 +4,18 @@ import csv
 connection = sqlite3.connect("./db/database.db")  # connect to your DB
 cursor = connection.cursor()  # get a cursor
 
-with open('./attachments/jokes.csv','r',newline='',encoding='utf-8') as csvfile:
-    reader=csv.reader(csvfile,delimiter='|', skipinitialspace=True, quoting=csv.QUOTE_MINIMAL)
-    for row in reader:
-        cursor.execute(('INSERT INTO jokes(jokes) VALUES (?)'),(str(row[1]),))
-        connection.commit()
+#with open('./attachments/jokes.csv','r',newline='',encoding='utf-8') as csvfile:
+#    reader=csv.reader(csvfile,delimiter='|', skipinitialspace=True, quoting=csv.QUOTE_MINIMAL)
+#    for row in reader:
+#        cursor.execute(('INSERT INTO jokes(jokes) VALUES (?)'),(str(row[1]),))
+#        connection.commit()
 
 
 
-cursor.execute("SELECT jokes OVER() AS number FROM jokes")  # execute a simple SQL select query
+cursor.execute("SELECT jokes, ROW_NUMBER() OVER() AS number FROM jokes")  # execute a simple SQL select query
 jobs = cursor.fetchall()  # get all the results from the above query
 cursor.close()
 connection.close()
 
-print(jobs)
+for i in jobs:
+    print(i)
