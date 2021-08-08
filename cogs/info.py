8ee80ff64f2a90d8a01f87f5e3a7096d636a011a
Marcelo Moreira <spl1ce5225@gmail.com>
2021-08-08 13:23:04 +0100

    added a better footer in info

--- cogs/info.py
+++ cogs/info.py
@@ -19,7 +19,7 @@ class Info(commands.Cog, description='Stuff that will display information.'):
         embed=discord.Embed(description=f'Bob was created by Gonçalo Francisco.\nThe discord bot was created by {owner.mention} (spl1ce#5225).',colour=0xf2f2f2,timestamp=datetime.utcnow())
         embed.set_author(name='Bob.',icon_url=self.logo)
         embed.set_thumbnail(url=self.logo)
-        embed.set_footer(text=f'Requested by {ctx.author.name}#{ctx.author.discriminator}')
+        embed.set_footer(text=f'If you find any bug report it to spl1ce#5225.')
         embed.add_field(name='Servers',value=len(self.bot.guilds))
         embed.add_field(name='Users',value=len(self.bot.users))
         embed.add_field(name='Invite',value=f'[Invite]({self.link})')
