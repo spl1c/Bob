b98666c6ba7f31c773022eaf9960bb42fd1f46b1
spl1c <spl1ce5225@gmail.com>
2021-09-10 04:38:21 +0100

    rich presence better

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ def get_prefix(bot, message):
     else:
         return commands.when_mentioned_or(str(prefix[0]))(bot, message)
 
-bot = commands.Bot(command_prefix=get_prefix, intents=intents)
+bot = commands.Bot(command_prefix=get_prefix, activity=discord.Activity(type = discord.ActivityType.playing,name='.help | Bob.'), intents=intents)
 
 
 def innit(self,bot):
@@ -36,7 +36,6 @@ def innit(self,bot):
 @bot.event
 async def on_connect():
     print('Bob is online!')
-    await bot.change_presence(activity=discord.Activity(type = discord.ActivityType.playing,name='on a Raspberry Pi 4 Model B!'))
 
 
 @commands.is_owner()
